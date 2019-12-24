24f692424d72edb4ee3003689a3effb5bc7d1be0
Nermin Kaya <nermin.kaya.sau@gmail.com>
2019-12-24 23:38:40 +0300

    Adding Files

---
+++ src/thread.java
@@ -0,0 +1,52 @@
+class producer extends Thread {
+    data d;
+
+    public producer(data d) {
+        this.d = d;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 10; i++) {
+            synchronized (d) {
+                d.deger++;
+                System.out.println("uretici: " + d.deger);
+            }
+        }
+    }
+}
+
+class consumer implements Runnable {
+    data d;
+
+    public consumer(data d) {
+        this.d = d;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 10; i++) {
+            synchronized (d) {
+                d.deger--;
+                System.out.println("tuketici: " + d.deger);
+            }
+        }
+    }
+}
+
+class thread {
+
+    public static void main(String args[]) {
+        data d = new data();
+        d.deger = 100;
+        producer p = new producer(d);
+        consumer c = new consumer(d);
+        Thread t = new Thread(c);
+        p.start();
+        t.start();
+    }
+}
+
+class data {
+    int deger=0;
+}
